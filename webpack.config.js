802a651fb428151277adcc100e4b57dfe1e8031a
Saad Hussain <saadhussain1994@gmail.com>
2019-03-03 12:50:22 -0600

    Initial template

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const outputDirectory = 'dist';
+const port = process.env.PORT || 8080;
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client/js/index.js'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': `http://localhost:${port}`
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
